032e7a3b5045bacfb8b2cdf9e9d2c33bb466146f
sepyangunawan <sepyangunawan@gmail.com>
2026-04-16 16:55:29 +0700

    feat(model): add search scope to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,6 +55,12 @@ class User extends Authenticatable
         ];
     }
 
+    public function scopeSearch($query, $search)
+    {
+       return $query->where('name', 'like', "%$search%")
+            ->orWhere('email', 'like', "%$search%");
+    }
+
     public function headOfFamily()
     {
         return $this->hasOne(HeadOfFamily::class);
