331dbf7d6c3094c2491743addf74ff648d0c0f21
sepyangunawan <sepyangunawan@gmail.com>
2026-04-16 16:54:27 +0700

    feat(api): create UserController and UserResource for API response

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ResponseHelper;
+use Illuminate\Http\Request;
+use App\Interface\UserRepositoryInterface;
+
+class UserController extends Controller
+{
+    private UserRepositoryInterface $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        Try{
+            $users = $this->userRepository->getAll(
+                $request->search,
+                $request->limit, true
+            );
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500 );
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
